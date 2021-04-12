2767e0d36f3c4067b6180d23b7732392f78575e1
cleberjamaral <cleberjamaral@gmail.com>
2021-04-12 00:20:30 -0300

    Discussion about sum of powers

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,4 +1,4 @@
-from manimlib import *
+from manim import *
 import numpy as np
 
 # To watch one of these scenes, run the following:
@@ -26,43 +26,62 @@ class SemiBox(VGroup):
 
 class DecimalSystem(Scene):
     def construct(self):
-        dec1 = Tex(r"1*100", r" + ", r"4*10", r" + ", r"7*1", r" = 147")
-        self.play(Write(dec1))
+        target_number = 147
+
+        dec0 = Integer(1)
+        self.play(
+            ChangeDecimalToValue(dec0, target_number)
+        )
         self.wait()
-        self.play(dec1.animate.shift(UP))
+        self.play(dec0.animate.shift(UP))
         self.wait()
 
-        dec2 = Tex(r"1*",r"10^2", r" + ", r"4*",r"10^1", r" + ", r"7*",r"10^0", r" = 147")
+        dec1 = Tex(
+            r"1*",r"100", r"+",
+            r"4*",r"10", r"+",
+            r"7*",r"1", r" = 147"
+        )
+        self.play(Write(dec1))
+        self.wait()
+        self.play(
+            dec0.animate.shift(UP),
+            dec1.animate.shift(UP)
+        )
+        self.wait()
+
+        dec2 = Tex(
+            r"1*",r"$10^2$", r"+",
+            r"4*",r"$10^1$", r"+",
+            r"7*",r"$10^0$", r" = 147"
+        )
         self.play(Write(dec2))
         self.wait()
         self.play(
+            dec0.animate.shift(UP),
             dec1.animate.shift(UP),
             dec2.animate.shift(UP)
         )
         self.wait()
 
-        dec3 = Tex(r"1~centena", r" + ", r"4~dezenas", r" + ", r"7~unidades", r" = 147")
+        dec3 = Tex(
+            r"1~",r"centena", r"+",
+            r"4~",r"dezenas", r"+",
+            r"7~",r"unidades", r" = 147")
         self.play(Write(dec3))
         self.play(
             dec3.animate.shift(ORIGIN)
         )
         self.wait()
 
-        dec1.set_color_by_tex("1*100",RED)
-        dec1.set_color_by_tex("4*10",BLUE)
-        dec1.set_color_by_tex("7*1",GREEN)
-        dec2.set_color_by_tex("1*",RED)
-        dec2.set_color_by_tex("10^2",RED)
-        dec2.set_color_by_tex("4*",BLUE)
-        dec2.set_color_by_tex("10^1",BLUE)
-        dec2.set_color_by_tex("7*",GREEN)
-        dec2.set_color_by_tex("10^0",GREEN)
-        dec3.set_color_by_tex("1~centena",RED)
-        dec3.set_color_by_tex("4~dezenas",BLUE)
-        dec3.set_color_by_tex("7~unidades",GREEN)
+        colors = [ORANGE,PINK,PURPLE,GRAY,RED,BLUE,GREEN]
+        for nn in reversed(range(0,len(dec1)//3)):
+            dec1[1+(nn*3)].set_color(colors[-nn])
+            dec2[1+(nn*3)].set_color(colors[-nn])
+            dec3[1+(nn*3)].set_color(colors[-nn])
         self.wait()
 
         self.play(
+            FadeOut(dec0),
             FadeOut(dec1),
             FadeOut(dec3),
             dec2.animate.shift(ORIGIN).scale(1.5)
@@ -74,26 +93,131 @@ class DecimalSystem(Scene):
         dec2.set_color_by_tex("7*",WHITE)
         self.wait()
 
-        rect1 = SurroundingRectangle(dec2[0], color = YELLOW)
-        rect2 = SurroundingRectangle(dec2[3], color = YELLOW)
-        rect3 = SurroundingRectangle(dec2[6], color = YELLOW)
+        rects = []
+        for nn in range(0,len(dec2)//3):
+            rect = SurroundingRectangle(dec2[1+(nn*3)], color = YELLOW)
+            rects.append(rect)
+        self.play(*list(map(Create, rects)))
+
+        self.wait(2)
+
+        arrow = DoubleArrow(ORIGIN, 5*RIGHT).move_to(ORIGIN)
         self.play(
-            ShowCreation(rect1),
-            ShowCreation(rect2),
-            ShowCreation(rect3),
+            Create(arrow),
         )
+        tleft = Tex("Mais~significativo").next_to(arrow, LEFT)
+        tright = Tex("Menos~significativo").next_to(arrow, RIGHT)
+        self.play(
+            Create(tleft),
+            Create(tright)
+        )
+        self.wait(2)
 
         self.play(
-            FadeOut(dec2[0]),
-            FadeOut(dec2[3]),
-            FadeOut(dec2[6]),
-            FadeOut(dec2[8])
+            *list(map(FadeOut, rects)),
+            dec2.animate.shift(UP).scale(0.667),
+            FadeOut(arrow),
+            FadeOut(tleft),
+            FadeOut(tright)
+        )
+
+        bin0 = Tex(f"${target_number:d}$" , "~=~", f"0b{target_number:b}")
+        self.play(
+            Create(bin0),
+        )
+        self.wait(2)
+
+        bin1 = Tex(
+            r"1*", r"128", r"+",
+            r"0*", r"64", r"+",
+            r"0*", r"32", r"+",
+            r"1*", r"16", r"+",
+            r"0*", r"8", r"+",
+            r"0*", r"4", r"+",
+            r"1*", r"2", r"+",
+            r"1*", r"1", "~=~", "0b", f"${target_number:b}$"
+        )
+        self.play(
+            bin0.animate.shift(UP),
+        )
+        self.wait(6)
+        self.play(
+            dec2.animate.shift(UP),
+            bin0.animate.shift(UP),
+            bin1.animate.shift(UP)
+        )
+        self.wait(4)
+
+        bin2 = Tex(
+            r"1*", r"$2^7$", r"+",
+            r"0*", r"$2^6$", r"+",
+            r"0*", r"$2^5$", r"+",
+            r"1*", r"$2^4$", r"+",
+            r"0*", r"$2^3$", r"+",
+            r"0*", r"$2^2$", r"+",
+            r"1*", r"$2^1$", r"+",
+            r"1*", r"$2^0$", "~=~", "0b", f"${target_number:b}$"
+        )
+        self.play(Write(bin2))
+        self.wait(4)
+
+        for nn in reversed(range(0,len(bin1)//3)):
+            bin1[1+(nn*3)].set_color(colors[-nn])
+            bin2[1+(nn*3)].set_color(colors[-nn])
+        self.wait(3)
+
+        recbs = []
+        for nn in range(0,len(bin2)//3):
+            recb = SurroundingRectangle(bin2[1+(nn*3)], color = YELLOW)
+            recbs.append(recb)
+        self.play(*list(map(Create, recbs)))
+        self.wait(3)
+
+        bin = bin0[2].copy()
+        bin.generate_target()
+        bin.target.move_to(DOWN)
+        self.play(
+            MoveToTarget(bin)
         )
         self.wait()
 
+        bin_notation = Tex(r"0b")
+        bin_decomposed = Tex(r"1001", r"~~", r"0011")
+        bin_decomposed.next_to(bin_notation, RIGHT)
+        bin_group = VGroup(bin_notation, bin_decomposed)
+        bin_group.next_to(bin, DOWN)
+        self.play(
+            Transform(bin, bin_group),
+        )
+        self.wait(2)
+
+        udb1 = Underline(bin_decomposed[0], color = YELLOW)
+        udb2 = Underline(bin_decomposed[2], color = YELLOW)
+        self.play(
+            Create(udb1),
+            Create(udb2),
+        )
+        text_nibbles = Tex("Nibbles")
+        text_nibbles.set_color(YELLOW)
+        text_nibbles.next_to(bin_group, DOWN * 2)
+        self.play(
+            Create(text_nibbles)
+        )
+        self.wait(2)
+
+        rec_byte = SurroundingRectangle(bin_decomposed, color = BLUE, buff=MED_SMALL_BUFF)
+        self.play(
+            Create(rec_byte)
+        )
+        text_byte = Tex("Byte")
+        text_byte.set_color(BLUE)
+        text_byte.next_to(bin_group, RIGHT * 2)
+        self.play(
+            Create(text_byte)
+        )
+
 
 class DecToBin(Scene):
-
     def print_result(self, dividend, divisor, dd, dv, rlist):
         quotient = dividend // divisor
         remainder = dividend % divisor
@@ -107,18 +231,21 @@ class DecToBin(Scene):
         ndv = Tex(str(divisor)).next_to(qu, RIGHT, buff=MED_SMALL_BUFF)
         if quotient >= divisor:
             semi_box = SemiBox(ndv, color = YELLOW)
-            self.play(ShowCreation(semi_box))
+            self.play(Create(semi_box))
             self.play(Write(ndv))
 
         return quotient, qu, ndv
 
-    def show_division(self, dividend, divisor):
+    def construct(self):
+        dividend = 67
+        divisor = 2
+
         dd = Tex(str(dividend))
         self.play(Write(dd))
-        self.play(dd.to_corner, UP+LEFT)
+        self.play(dd.animate.to_corner(UP + LEFT))
         dv = Tex(str(divisor)).next_to(dd, RIGHT, buff=MED_SMALL_BUFF)
         semi_box = SemiBox(dv, color = YELLOW)
-        self.play(ShowCreation(semi_box))
+        self.play(Create(semi_box))
         self.play(Write(dv))
 
         rlist = []
@@ -128,11 +255,11 @@ class DecToBin(Scene):
         bin = qu.copy()
 
         rect = SurroundingRectangle(qu, color = PINK)
-        self.play(ShowCreation(rect))
+        self.play(Create(rect))
         clist = [bin]
         for r in reversed(rlist):
             rect = SurroundingRectangle(r, color = PINK)
-            self.play(ShowCreation(rect))
+            self.play(Create(rect))
             rem = r.copy()
             clist.append(rem)
 
@@ -159,10 +286,11 @@ class DecToBin(Scene):
         msb.set_color(GREEN)
         tmsb.set_color(GREEN)
         self.play(
-            ShowCreation(amsb),
-            ShowCreation(msb),
-            ShowCreation(tmsb),
-            tmsb.animate.shift(LEFT)
+            Create(amsb),
+            Create(msb),
+            Create(tmsb),
+            tmsb.animate.shift(LEFT),
+            clist[0].animate.scale(1.5)
         )
 
         alsb = Arrow(start=UP, end=DOWN, buff=SMALL_BUFF).next_to(clist[-1], DOWN)
@@ -174,89 +302,42 @@ class DecToBin(Scene):
         lsb.set_color(BLUE)
         tlsb.set_color(BLUE)
         self.play(
-            ShowCreation(alsb),
-            ShowCreation(lsb),
-            ShowCreation(tlsb),
-            tlsb.animate.shift(RIGHT*1.5)
+            Create(alsb),
+            Create(lsb),
+            Create(tlsb),
+            tlsb.animate.shift(RIGHT*1.5),
+            clist[-1].animate.scale(0.75)
         )
 
-        for c in clist:
-            c.save_state()
-            self.play(
-                c.animate.scale(((len(clist) - clist.index(c)) / len(clist)) * 2),
-            )
-        for c in clist:
-            self.play(
-                c.restore
-            )
+        self.wait()
 
+class Notation(Scene):
     def construct(self):
-        self.show_division(47,2)
-
-class ShowWords(Scene):
-    def construct(self):
-        words = VGroup(
-            TexText("Machine", "learning").set_color(GREEN),
-            TexText("Neural network").set_color(BLUE),
-        )
-        words[0].save_state()
-        words[0].shift(DOWN)
-        words[0].fade(1)
-
-        self.wait()
-
+        dividend = 67
+        result = Tex(str(dividend), "~=~", f"{dividend:b}")
         self.play(
-            words[0].restore
+            result.animate.shift(UP*3),
+            Write(result)
         )
+        self.wait(2)
 
-        self.wait()
-
-        #self.play(
-        #    words[0].shift,
-        #    FadeIn(words[1]),
-        #)
-
-        self.play(words.to_corner, UP+RIGHT)
-
-        self.words = words
-        self.wait()
-        #self.play(
-        #    ReplacementTransform(
-        #        VGroup(self.words[1].copy())
-        #    ),
-        #    run_time = 1
-        #)
-
-        equation = Tex(
-            "\\textbf{a}_{l+1}", "=",
-            "\\sigma(",
-                "W_l", "\\textbf{a}_l", "+", "b_l",
-            ")"
-        )
-        equation.set_color_by_tex_to_color_map({
-            "\\textbf{a}" : GREEN,
-        })
-        equation.move_to(UP+LEFT)
-        equation.to_edge(UP)
-
-        self.play(Write(equation, run_time = 2))
-        self.wait()
-
-        self.equation = equation
-
-        self.wait()
-
-        word = self.words[0][1].copy()
-        rect = SurroundingRectangle(word, color = YELLOW)
-
-        word_group = VGroup(word, rect)
-        word_group.generate_target()
-        word_group.target.move_to(self.equation, LEFT)
-        word_group.target[0].set_color(YELLOW)
-        word_group.target[1].set_stroke(width = 0)
-
-        self.play(ShowCreation(rect))
+        result2 = Tex(f"${dividend:d}$", "$_{10}$", "~=~", f"${dividend:b}_2$")
         self.play(
-            FadeOut(self.equation),
-            MoveToTarget(word_group),
+            Transform(result, result2)
         )
+        self.wait(2)
+
+        result3 = Tex(f"$0d{dividend:d}$", "~=~",  f"$0b{dividend:b}$")
+        self.play(
+            FadeOut(result),
+            Transform(result2, result3)
+        )
+        self.wait(2)
+
+        result4 = Tex(f"${dividend:d}$", "~=~", f"$0b{dividend:b}$")
+        self.play(
+            FadeOut(result2),
+            Transform(result3, result4)
+        )
+        self.play(FadeOut(result3))
+        self.wait(3)
